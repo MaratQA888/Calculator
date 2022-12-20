976d028fc110f3818730904ff363dc89e8cbcb94
MaratQA888 <yennifreak@icloud.com>
2022-12-20 12:28:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+      int ticketprice = 2000;
+      int rublespermile = 20;
+      int bonus = ticketprice/rublespermile;
+
+        System.out.println(bonus + " бонусных миль начислено");
+
+    }
+}
